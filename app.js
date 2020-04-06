ce3dcda2fd58e689c88e5863905f68e16b2ae383
azevedorafael <you@example.com>
2020-04-05 22:06:05 -0300

    Handling POST Requests

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 
 const port = 3000;
 
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //   res.status(200).json({ message: 'Hello from the server side!' });
 // });
@@ -25,6 +27,26 @@ app.get('/api/v1/tours', (req, res) => {
       tours
     }
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      });
+    }
+  );
 });
 
 app.listen(port, () => {
