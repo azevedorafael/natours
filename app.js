7407205f190652174e74e055b686ce3f5b1d0280
azevedorafael <you@example.com>
2020-04-05 23:55:42 -0300

    Handling PATCH Requests

--- app.js
+++ app.js
@@ -65,6 +65,22 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find(el => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour
+    }
+  });
+});
+
 app.listen(port, () => {
   console.log(`App runing on port ${port}...`);
 });
