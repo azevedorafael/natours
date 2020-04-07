4b24954565b2fe64a024a0de01aaca4429b8c3a4
azevedorafael <you@example.com>
2020-04-06 21:42:05 -0300

    Create middleware

--- app.js
+++ app.js
@@ -7,13 +7,25 @@ const port = 3000;
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 👏');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours,
